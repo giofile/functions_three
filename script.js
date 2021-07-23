77541249c22f8f3d5e1242ea5262559ba15943fc
giofile <77681874+giofile@users.noreply.github.com>
2021-07-23 15:31:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const berekening = function (num1, num2) {
+    const som = (num1*num1) + (num2* num2)
+    return som
+    
+}
+
+console.log(berekening(3,4));
+
+
+function berekeningTwee (num1, num2){
+    const somTwee = (num1*num1) + (num2*num2) 
+    return somTwee
+}
+
+console.log(berekeningTwee(3,4));
+
+
+const berekeningDrie = (num1, num2) => {
+    const somDrie = (num1*num1) + (num2*num2)
+    return somDrie
+}
+
+console.log(berekeningDrie(3,4));
